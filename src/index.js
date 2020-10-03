912b073ec35c7d1d3a6af9aa83d23f1c2b1225fe
Nedomorf <danikp01@,ail.ru>
2020-10-03 13:33:33 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let res = 0;
+    if (array && array.length > 0) res = Math.min(...array);
+    return res;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let res = 0;
+    if (array && array.length > 0) res = Math.max(...array);
+    return res;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let sum = 0;
+    if (array && array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+    }
+    let res = 0;
+    if (sum !== 0) res = (sum / array.length);
+    return res;
 }
